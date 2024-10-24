2bd95b666debe9729de887f891b1883642d51f18
ShauThongHo <161423722+ShauThongHo@users.noreply.github.com>
2024-10-24 11:12:39 +0800

    update

--- Welcome1.java
+++
@@ -1,7 +0,0 @@
-//9B240002A
-public class Welcome1 { 
-    public static void main(String[] args) { 
-      // Display message Welcome to Java! on the console
-      System.out.println("Welcome to Java!");
-    }
-  }
