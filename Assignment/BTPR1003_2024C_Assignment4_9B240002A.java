44278eaacb7f5d0ea37c786c98f9fea6ccb7f6f9
ShauThongHo <161423722+ShauThongHo@users.noreply.github.com>
2024-11-28 21:00:30 +0800

    Create BTPR1003_2024C_Assignment4_9B240002A.java

---
+++ Assignment/BTPR1003_2024C_Assignment4_9B240002A.java
@@ -0,0 +1,17 @@
+//HO SHAU THONG
+//9B240002A
+//BoS24-A1
+//7 Dec 2024
+/*Write a method that computes the sum of the digits in an integer. 
+Use the following method header: 
+public static int sumDigits(int n) 
+
+Write a test program that prompts the user to enter an integer then displays the sum of all its digits. 
+
+For example, sumDigits(234) returns 9 (= 2 + 3 + 4). */
+
+import java.util.Scanner;
+
+public class BTPR1003_2024C_Assignment4_9B240002A {
+    
+}
