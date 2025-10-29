e69971ab2b8eddd296f8ca12e0e1995316d5aa2c
ShauThongHo <161423722+ShauThongHo@users.noreply.github.com>
2025-10-29 10:55:08 +0800

    .

---
+++ cal.java
@@ -0,0 +1,13 @@
+class cal{
+    public static void main(String[] args){
+        int ori = 6100;
+        int scope = 100;
+        for(int j = 1 , ori2 = ori + scope; j < scope; j++){
+            System.out.println("yt250262126603" + (ori2 - j));
+        }
+        System.out.println("yt250262126603" + ori);
+        for(int j = 1; j < scope; j++){
+            System.out.println("yt250262126603" + (ori - j));
+        }
+    }
+}
---
+++ cal2.java
@@ -0,0 +1,10 @@
+public class cal2 {
+    public static void main(String[] args){
+        int ori = 169546;
+        int scope = 100;
+          
+        for(int j = 1 , ori2 = ori - 900 ; j < scope+1; j++){
+            System.out.println("XMX3GT" + (ori2 - j));
+        }
+    }
+}
